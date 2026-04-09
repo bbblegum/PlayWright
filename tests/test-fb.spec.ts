079c21c7bb58d513c7eaa5b4a5f09c00893826d0
bbblegum <38193806+bbblegum@users.noreply.github.com>
2026-04-09 15:38:51 -0600

    Update test-fb.spec.ts

--- tests/test-fb.spec.ts
+++ tests/test-fb.spec.ts
@@ -3,7 +3,7 @@ import { test } from '@playwright/test';
 test('face', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
 
-  await page.locator('//*[@id="_R_64qjbjb9pb6amH1_"]').fill('ale.mayo@outlook.com');
+  await page.locator('//*[@id="_R_64qjbjb9pb6amH1_"]').fill('email@email.com');
   await page.locator('//*[@id="_R_66qjbjb9pb6amH1_"]').fill('password');
   await page.getByRole('button', { name: 'Log In' }).click();
 
